f1d869cbc72ed63d0aa90b49756c84af17d0881a
Eric M. David <ericdavid365@gmail.com>
2015-11-20 15:27:54 -0600

    second initial commit

---
+++ lib/binary/search/tree.rb
@@ -0,0 +1,9 @@
+require "binary/search/tree/version"
+
+module Binary
+  module Search
+    module Tree
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/binary/search/tree/version.rb
@@ -0,0 +1,7 @@
+module Binary
+  module Search
+    module Tree
+      VERSION = "0.0.1"
+    end
+  end
+end
